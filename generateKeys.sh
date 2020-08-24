9366d161938aed086fc9727e4a75d670cdb116c8
Matthew Swezey <matthew.swezey@getupside.com>
2020-08-24 14:04:38 -0500

    ✨jazz up the generate keys

--- generateKeys.sh
+++ generateKeys.sh
@@ -9,15 +9,22 @@ if which docker > /dev/null
         # exit 1
 
         echo "Installing Docker... One moment please..."
-        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
         sudo apt update
         apt-cache policy docker-ce
-        sudo apt install docker-ce
-        sudo systemctl status docker
+        sudo apt install -y docker-ce
         echo "Adding user to docker group..."
+
+        if grep -q $group /etc/group
+        then
+            echo "group exists"
+        else
+            sudo groupadd docker
+        fi
         sudo usermod -aG docker ${USER}
+        newgrp docker
         echo "Docker setup complete! 💯"
 fi
 
@@ -30,3 +37,5 @@ docker run parity/subkey:2.0.0-rc6 inspect-key --scheme Sr25519 "$mnemonic"
 
 printf "Now update the aura.json & gran.json files! (under ./keystore)\n"
 printf "WIP - this will continue to be automated further. -Matt\n"
+
+printf "To uninstall docker run: 'sudo apt-get remove docker docker-engine docker.io containerd runc docker-ce-cli'\n"
