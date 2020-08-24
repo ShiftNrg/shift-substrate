d6b5880baa4f65e1a18af6d6ccd64ac988a10371
Matthew Swezey <matthew.swezey@getupside.com>
2020-08-24 17:52:21 -0500

    use shift's network for key generation

--- generateKeys.sh
+++ generateKeys.sh
@@ -10,23 +10,31 @@ useDocker() {
     fi
 
     printf "Generating Aura Keys\n"
-    docker run parity/subkey:2.0.0-rc6 generate --scheme Sr25519
+    docker run parity/subkey:2.0.0-rc6 generate -n "shift" --scheme Sr25519
 
     printf "Generating Granpa Keys...\n"
     read -p 'Paste Mnemoic: ' mnemonic
-    docker run parity/subkey:2.0.0-rc6 inspect-key --scheme Sr25519 "$mnemonic"
+    docker run parity/subkey:2.0.0-rc6 inspect-key -n "shift" --scheme Ed25519 "$mnemonic"
 
     printf "Now update the aura.json & gran.json files! (under ./keystore)\n"
     printf "WIP - this will continue to be automated further. -Matt\n"
 }
 
 useSubkey() {
+    if which subkey > /dev/null
+        then
+            echo "Subkey is installed...Will continue"
+        else
+            echo "Please install subkey and run again"
+            echo "'cargo install --force --git https://github.com/paritytech/substrate subkey'"
+            exit 1
+    fi
     printf "Generating Aura Keys\n"
-    subkey generate --scheme Sr25519
+    subkey generate -n "shift" --scheme Sr25519
 
     printf "Generating Granpa Keys...\n"
     read -p 'Paste Mnemoic: ' mnemonic
-    subkey inspect-key --scheme Sr25519 "$mnemonic"
+    subkey inspect-key -n "shift" --scheme Ed25519 "$mnemonic"
 }
 
 echo -n "Generate Keys using Docker (d) or Subkey (s)? ([d]/s) (Note: subkey must already be installed): "
