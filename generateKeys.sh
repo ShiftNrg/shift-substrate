00a96ff94d753356fef7ebc1774c0ee5b4bbc28f
Matthew Swezey <matthew.swezey@getupside.com>
2020-08-24 17:54:57 -0500

    output keys as JSON

--- generateKeys.sh
+++ generateKeys.sh
@@ -10,11 +10,11 @@ useDocker() {
     fi
 
     printf "Generating Aura Keys\n"
-    docker run parity/subkey:2.0.0-rc6 generate -n "shift" --scheme Sr25519
+    docker run parity/subkey:2.0.0-rc6 generate -n "shift" --scheme Sr25519 --output-type Json
 
     printf "Generating Granpa Keys...\n"
     read -p 'Paste Mnemoic: ' mnemonic
-    docker run parity/subkey:2.0.0-rc6 inspect-key -n "shift" --scheme Ed25519 "$mnemonic"
+    docker run parity/subkey:2.0.0-rc6 inspect-key -n "shift" --scheme Ed25519 --output-type Json "$mnemonic"
 
     printf "Now update the aura.json & gran.json files! (under ./keystore)\n"
     printf "WIP - this will continue to be automated further. -Matt\n"
@@ -30,11 +30,11 @@ useSubkey() {
             exit 1
     fi
     printf "Generating Aura Keys\n"
-    subkey generate -n "shift" --scheme Sr25519
+    subkey generate -n "shift" --scheme Sr25519 --output-type Json
 
     printf "Generating Granpa Keys...\n"
     read -p 'Paste Mnemoic: ' mnemonic
-    subkey inspect-key -n "shift" --scheme Ed25519 "$mnemonic"
+    subkey inspect-key -n "shift" --scheme Ed25519 --output-type Json "$mnemonic"
 }
 
 echo -n "Generate Keys using Docker (d) or Subkey (s)? ([d]/s) (Note: subkey must already be installed): "
